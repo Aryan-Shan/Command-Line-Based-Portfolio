8ba9f23525deccb7c92df3b711cf78d711024a66
Aryan-Shan <142845473+Aryan-Shan@users.noreply.github.com>
2024-06-12 22:42:52 +0530

    minor changes

--- script.js
+++ script.js
@@ -288,7 +288,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function displayTitle() {
         const titleText = "Aryan Shandilya";
-        const footerText = " | A.S.";
+        const footerText = " | illic 'magis'";
         const titleElement = document.getElementById('title');
         const footerElement = document.getElementById('footer');
         typeEffect(titleElement, titleText, 100);
